afee74fbaaeacb541de23911f764abdabc47ba16
danroyal001 <danroyaleffiong@gmail.com>
2024-02-04 16:06:05 +0100

    remove incompatble page navigation

--- 1_🏡_Homepage.py
+++ 1_🏡_Homepage.py
@@ -22,8 +22,6 @@ search_term = st.text_input("Search for a product",
 
 submit = st.button("Submit")
 if submit:
-    st.switch_page('pages/2_🔎_Search_Results.py')
-
     st.session_state["search_term"] = search_term
     st.write("You have entered: ", search_term)
 
