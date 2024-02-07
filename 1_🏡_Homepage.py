ba00e5bd8029fa24c17ddc4acd75b9141eb60461
dorafina <thessyzilla@gmail.com>
2024-02-07 18:18:17 +0100

    fixing the image url

--- 1_🏡_Homepage.py
+++ 1_🏡_Homepage.py
@@ -1,113 +1,113 @@
-import streamlit as st
+import products as st
 
 # Define mock product data with placeholder images
 products = {
     180158: {
         'name': "ProCom 80,000 BTU Portable Single Convection Heater",
         'description': "ProCom Heating's Portable Convection Outdoor Propane Heaters, also referred to as \"trash can\" heaters in the trade, offer an inexpensive solution for temporary space heating especially for applications where there is no electricity. Compact, lightweight and clean burning, these heaters also feature a 360 heating radius which allows quick and efficient hot air distribution. These commercial grade heaters offer a perfect solution for temporary heating jobs such as garages, construction sites, warehouses, agricultural and industrial applications.California residents: see&nbsp;Proposition 65 information80,000 BTU variable convection heaters3 heat settings - 40,000, 60,000 and 80,000 BTUCirculates convection heat 360 in a degree radiusHeats up to 1,800 sq. ft.No electricity requiredPiezo matchless ignitionCSA  certifiedIncludes 10 ft. hose and regulatorHome Depot Protection Plan:",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://www.amazon.ca/ProCom-PCC80V-Portable-Single-Convection/dp/B00IPUFS4U"
     },
 
     111110: {
         'name': "HDX 4 ft. x 50 ft. 14-Gauge Green PVC-Coated Welded Wire",
         'description': "The HDX 4 ft. x 50 ft. Vinyl-Coated Welded Wire is made of galvanized and welded steel for long-lasting use. The green PVC coating helps create an upscale appearance that can complement natural surroundings. The general purpose fence is ideal for property delineation or the temporary confinement of non-aggressive animals.Galvanized and welded steel construction for long-lasting useMesh size: 3 in. x 2 in.14-GaugeGreen PVC coating helps create an upscale appearance that can complement natural surroundingsBuying Guide:",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/KXiymCPcFukdesxf7"
     },
 
     102816: {
         'name': "Delta Classic 400 32 in. x 60 in. x 60 in. 3-Piece Direct-to-Stud Tub Surround in High Gloss White",
         'description': "The Classic 400 32 in. X 60 in. x 60 in. 3-Piece Direct-to-Stud Tub Surround in High Gloss White is designed to fit 32 in. x 60 in. x 60 in. alcoves. This wall set offers an attractive design with the strength and durability of acrylic. This wall set also features six corner shelves and one center shelf for maximum storage. Classic 400 bathing systems offer style and design with the strength and durability of acrylic material. ProCrylic is a durable, bright white, high-gloss acrylic that provides all of the benefits of acrylic without the need for the rough, abrasive fiberglass backing.Actual product dimensions 60 in. W x 32.5 in. D x 60.38 in. HFits alcoves 32 in. x 60 in. x 60 in.Pairs with aspiration 38 in. x 48 in. single threshold shower base in high gloss white (model # 40034L or 40034R - sold separately) for a complete showering unitPremium high-gloss non-porous acrylic surface is durable and easy to cleanDirect-to-stud installation helps to create a long-lasting and secure unit7 built-in shelves provide much-needed storage spaceBuilt-in nailing flange to help ease installation3-piece design is easy to transportTub surround wall onlyBright white finish matches porcelain fixturesEasy to clean surface resists mildewEasy to handle material with no fiberglass backingNo mortar bed required saves time and money",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/wcxVvLTsXrDrqu8i7"
     },
 
     153802: {
         'name': "SharkBite 3/4 in. Copper Crimp Rings (100-Pack)",
         'description': "The SharkBite barb connection system is the best option for connecting PEX tubing in water distribution systems when lower material cost is desired. Constructed of copper, the SharkBite copper crimp rings are used in conjunction with barb fittings and crimp tools. And with a wide range of fittings, manifolds, regulator, thermostatic, and ball valves available, the SharkBite barb connection system is perfect for your next plumbing project.CopperCrimp tool allows each joint to be checked with go/no-go gaugeUsed in conjunction with PEX tubing and barb fittings, valves, and manifoldsAvailable in 3/8 in., 1/2 in., 3/4 in., & 1 in. sizesTool required has interchangeable jaws that do not require calibration",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://www.sharkbite.com/us/en/pex-pipe/pex-cutters-accessories/copper-crimp-ring"
     },
 
     150321: {
         'name': "6 ft. x 8 ft. Pressure-Treated Pine Shadowbox Fence Panel",
         'description': "Use the 6 ft. x 8 ft. Pressure Treated Spruce Shadowbox Fence Panel to help provide privacy in your yard. This panel is made of solid unpainted spruce, and is treated to provide resistance to termites and fungal decay. It can also be stained or painted as you see fit.California residents: see&nbsp;Proposition 65 informationPressure treated for long lifeNatural wood appearance can be stained or painted to suit your locationDesigned to enhance the appearance of your yardPanel measures 8 ft. wideNote: Product may vary by store",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/2tuNGoTrvr2WQ8kSA"
     },
 
     189550: {
         'name': "Hampton Bay Mix &amp; Match Chrome Double Glass Orb Table Lamp-Title 20",
         'description': "Hampton Bay's Mix and Match lamp program allows you to create your own signature lamp style. Create your lamp by choosing a shade or base from 2-sizes, (A) accent or (B) table, then pick from an array of traditional, contemporary, or transitional styles. The numerous choices of shades and bases are the perfect opportunity to refresh an outdated lamp, or you can start from scratch and design a whole lamp that's right for you.Beautifully crafted in metal / poly for added eleganceMix & match for additional looks for all seasons, moods and decorUse 1-standard medium base 13 watt CFL bulb (included)Choose any matching size shade to finish your lookEasy to install - harp and finial included where neededHome Depot Protection Plan:",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/smPgibjL92FYh2rBA"
     },
 
     146952: {
         'name': "Hitachi 7/8 in. x 0.120 in. Full Round-Head Smooth Shank Electro Galvanized Wire Coil Roofing Nails (7,200-Pack)",
         'description': "The Full Round-Head Smooth Shank Wire Coil Electro-Galvanized Roofing Nails fit Hitachi NV45AB, NV45AB2, NV45AB2S, NV45AE and NV45AES roofing nailers. These nails have a smooth shank with an Electro-Galvanized finish. This item is 7/8 in. in length x 0.120 in. in diameter and comes in a (7,200-pack). Every Hitachi accessory is designed to the highest standards and is rigorously tested for both performance and durability. Since its inception, Hitachi has pioneered innovative technologies that have improved the quality of craftsmanship worldwide. Hitachi is a leader in power tool research and development and has achieved many firsts in the power tool industry. Today, Hitachi continues the tradition of innovation and engineering with new features in addition to classic quality.Full round-head wire coil electro-galvanized roofing nailsFits Hitachi NV45AB, NV45AB2, NV45AB2S, NV45AE and NV45AES roofing nailersNails have a smooth shank and an electro-galvanized finishHas a 7/8 in. length x 0.120 in. diameter and comes in a 7,200-pack",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/nhEaT38B2aUaEarT9"
     },
 
     129672: {
         'name': "Bell 1-Gang Weatherproof Box with 3 1/2 in. Outlets",
         'description': "BELL weatherproof boxes are designed for use in branch circuit wiring in wet, damp, or dry locations.  Boxes house receptacles, switches, and GFCI's.  BELL Boxes may also be used as a weatherproof junction box. Closure plugs and installation hardware included. Ground screw is installed.State of the art powder coat finish provides maximum weatherability and scratch resistanceRugged die cast aluminum constructionInternal hub threads comply with NEMA requirements and will accept all threaded fittings and threaded conduit8 box mounting options with detachable lugs2 in. deep18.3 cubic in. capacityNEMA 3R Compliant",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/Zw66NKkWp6E2G9FcA"
     },
 
-    167755: {
+    167756: {
         'name': "Rust-Oleum Universal 12 oz. All Surface Satin White Spray Paint and Primer in One",
         'description': "Rust-Oleum Universal 12 oz. All Surface Satin White Spray Paint and Primer in One is designed for diverse substrates and works on virtually any surface: vinyl, metal, plastic, fiberglass, concrete, glass, wicker and wood. Universal is suitable for interior or exterior environments. Oil based formula covers up to 35 sq. ft., protects surfaces from rust and dries to the touch in as little as 30 minutes. A specially designed nozzle eliminates finger fatigue, protects against dripping and sprays from any angle, even upside down.California residents: see&nbsp;Proposition 65 informationInterior/exterior use, designed for diverse substrates and works on virtually any surface: vinyl, metal, plastic, fiberglass, concrete, glass, wicker and woodRust and corrosion resistant for enhanced durabilityCovers up to 35 sq. ft.Satin finish offers a soft, low-sheen look for any surfaceErgonomic cap eliminates finger fatigue and offers 360-degree, any-angle spray technologyOil-based formula with excellent adhesion resists fading and chippingDries to the touch in 30 minutesPaint and primer in oneClean up with mineral spiritsActual paint colors may vary from on-screen and printer representationsSee below for the full line of Universal products",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/3MYoV5kD7RAa9AmQ9"
     },
 
     106723: {
         'name': "Hampton Bay 3-Light Crawley Oil Rubbed Bronze Vanity Fixture",
         'description': "The versatile Crawley vanity fixtures feature a classic timeless architecture that adds an element of distinction to any bathroom. The Oil Rubbed Bronze hand applied finish coordinates with most faucet and hardware manufacturers for easy selection. The fixtures can be mounted in either the up or down position depending on the consumer's choice. The white glass shades provides modern day clean feel with old world design.White glass shadesMount with glass up or downCoordinates with most bronze faucetsEasy to installOil rubbed bronze finishHome Depot Protection Plan:",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/yMUQitZbRCRM66UC7"
     },
 
     175434: {
         'name': "LR Resources Vivid Floral Brown and Grey with a Purple Haze 5 ft. x 7 ft. 9 in. Indoor Area Rug-DISCONTINUED",
         'description': "A Single Outlined Flower Rests on a Monochromatic Ground, Creating a Look that is both Modern and Dramatic. 100% Acrylic Pile, Hand-Tufted in China. Durable Rug with a Minimal Shed Construction. Adds Flair to Any Home Environment with Hook and Cut Pile TePile Height (approximate) 1/2\"100% Acrylic PileDurable, minimal-shed constructionRug pad recommendedSpot clean, blot spills immediately and vacuum regularly",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/SAtwqsByiguoZ1LG8"
     },
 
     173941: {
         'name': "Wiremold 500 and 700 Series 5-1/2 in. Open Base Extension Box",
         'description': "Legrand/Wiremold small surface metal raceway systems are the ideal choice for a homeowner that wants to extend circuits or communication cabling in their home. Surface metal raceway allows you to extend circuits, add fixtures or outlets without disturbing the drywall, plaster or insulation in your home. All parts are paintable to blend with room decor. The leading choice of professionals is now available for your DIY project.90_ twist elbow kit enables a turn around an outside corner while changing the raceway channel's orientationAccommodates fixtures with 2-3/4 in. 3-1/2 in. and 4-1/16 in. mounting patternsCompatible with both Legrand/Wiremold 500 series (B-series) and 700 series (BW series) racewaysIvory finishPaintable and stainable using latex based productsUL listedNote: product may vary by store",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/e14DTqiSbJMA7fz16"
     },
 
     188002: {
         'name': "Builders Edge 15 in. x 52 in. Louvered Vinyl Exterior Shutters Pair in #002 Black",
         'description': "Builders Edge decorative 15 in. x 52 in. Louvered Vinyl Exterior Shutters Pair in #002 Black (actual 14.5 in. x 51.75 in.) are designed with a deep wood-grain texture for the appearance of wood shutters without the maintenance worries of wood. Durable copolymer construction features molded-through color so there is no paint to ever scratch or flake. Our shutters install on any surface: wood, vinyl, stucco, brick, or fiber cement. Includes Shutter-Lok fasteners for installation. Limited lifetime warranty.Available in 20 maintenance-free accent colors or a separate paintable optionMatching color installation hardware includedDue to materials, widths and lengths are nominalProduct cannot be paintedSold in pairsAK, HI ship to store onlyLimited lifetime warranty",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/xFum1r3A3j9LHiW67"
     },
 
     195936: {
         'name': "Leaktite Premium 5-gal. Food Storage Container (10-Pack)",
         'description': "Leaktite's 5 Gal. premium storage containers are manufactured to be safe for food storage. The premium design is manufactured from high quality plastic and is tough enough to store nails and screws along with any standard home or food needs. Includes 10 containers. Lids are available.All pails have a sturdy wire bail with a plastic handgripStrong reinforcing ribs guarantee that the containers keep their shapeThe wall thickness is .090 in.Made with a tapered design, pails nest to save space, and they separate easilyLids are not included with the buckets",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/NgjNvpk34yvZiLNy9"
     },
 
     112517: {
         'name': "KOHLER Wheatland Self-Rimming Cast Iron 33x22x9.625 4-Hole Kitchen Sink in White",
         'description': "A fresh new take on traditional sink design, Wheatland offers innovative features in a stylish package. Crafted from enameled cast iron, this sink resists scratching, burning, and staining for years of beauty and reliable performance. Large/medium bowls allow you to keep clean and dirty dishes separate, and the large bowl is extra-spacious to accommodate your biggest pots and pans.Click here to explore the Strength, Style, and Soul, of KOHLER Enameled Cast Iron.36-inch minimum base cabinet widthLarge/medium bowls9-inch depthThree faucet holes with one accessory hole to the rightRear drain increases workspace in the sink and storage space underneathKOHLER enameled cast ironTop-mountClick here to learn more about kitchen sinks",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/JKxfCzbuBnz7CCTF7"
     },
 
     100480: {
         'name': "Cub Cadet 3X 26 in. 357cc 3-Stage Electric Start Gas Snow Blower with Power Steering and Heated Grips",
         'description': "Introducing the revolutionary new Cub Cadet 3X 3-Stage snow blower. These industry exclusive cuts through tough packed snow and ice like no other machine available today. The 3X's unique high-speed Induction Accelerator draws snow through the system, creating an effortless forward motion. No backups. No ride ups. No spill over. This new 3X technology easily cuts through deep snowfall up to 50% faster than 2-Stage snow blowers. There's no stopping the next generation of Cub Cadet snow blowers.California residents: see&nbsp;Proposition 65 informationIdeal snowfall range: 6 in. to 16 in.Ideal surfaces: smooth and gravel surfaces; flat to slight slope26 in. clearing width, 21 in. intake height357cc electric start Cub Cadet 4-cycle OHV engineClear snow faster: new 3X induction accelerator moves snow up to 50% faster than 2X snow throwersTrigger controlled power steering at your fingertips for unmatched control, effortless maneuverability and 1-hand operationHeated hand grips for added comfortRevolutionary cool blue skid shoes glide along surfaces for better maneuverability and won't rust or damage your driveway6 forward and 2 reverse speeds allow for easy snow clearing at your pacePush button electric start makes it incredibly easy to start in tough winter conditionsSingle-hand 4-way chute control adjusts the chute rotation and pitch with 1-hand for the ultimate snow throwing controlIn-dash headlight allows you to clear your driveway any time of the day or night15 in. x 5 in. X-TRAC tires provide solid traction in extreme weather conditionsThrows snow up to 40 ft.Use 5-Walt-30 engine oil for easy starting at all temperaturesYou're covered winter after winter with a premium 3-year limited residential and 1-year limited commercial warranty and 5-year limited auger gearboxHome Depot Protection Plan:",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/PscQUbqrne8nGBau8"
     },
 
     139340: {
         'name': "Martha Stewart Living 1-3/8 in. Wood Single Bracket in Antique Mahogany",
         'description': "Martha Stewart Living 1-3/8 in. Brackets decorate your window with the inherent beauty of natural wood. The natural beauty of wood brackets adds style to any decor. The brackets screw into a wall to securely attach a wood pole to the surface for hanging window treatments.Use with 1-3/8 in. wood poleInclude 1-pair wood brackets and mounting hardwareUse 1-bracket for every 4 ft. of pole lengthExclusively at the Home Depot",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/hj8PZT72AHR6jsjAA"
     },
 
     203654: {
         'name': "Fathead 20 in. x 32 in. Troy Polamalu Pittsburgh Steelers Wall Decal",
         'description': "Your  Fathead Jr. has all the fun action and real-life graphics of Real. Big. Fathead Wall Graphics in a more space-friendly size. Fathead Jr. fits all kinds of spaces including doors, fridges, windows, tables, dorm rooms, etc.  And best of all, it is completely repositionable, and will not leave any residue on your wall.Rich, vivid, precision cut imagesEasy to apply: Just peel and stickMoveable and safe for wallsExtra coordinating images includedClick here to learn more about wall decor",
-        'image_url': "https://via.placeholder.com/150"
+        'image_url': "https://images.app.goo.gl/RU2w68w9XkCpDuHt9"
     },
 
     115326: {
