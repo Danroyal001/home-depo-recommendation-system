741d3097d8508fa6e9ba3c4ce653b91ea9416a85
danroyal001 <danroyaleffiong@gmail.com>
2024-02-19 00:07:30 +0100

    _

--- file.py
+++
@@ -1,8 +0,0 @@
-product = [{'product_uid': '174050', 
-            'product_name': '2 gal. Deck and Home Sprayer', 
-            'product_description': 'The Deck and Home Sprayer is ideal for cleaning and sealing decks. This sprayer can also be used as a lawn and garden applicator and for insect control. Included is a high performance foaming nozzle that produces a thick visible penetrating foam. California residents: see Proposition 65 information 4 nozzle system-includes poly adjustable, 2 flat fans and a high performance foaming nozzle 36 in. reinforced hose provides user with longer reach Custom designed shut-off with lock-on feature for continuous spraying Large funnel fill top',
-            'product_image_url': 'https://www.tenaquip.com/images/large/n/no293.webp?1631568416', 
-            'product_image_visual_description': 'A 2 gallon deck and home sprayer with multiple nozzles for various spraying applications.',
-            'search_relevance_score': 'High', 'reason_for_recommendaion': 'The 2 gallon Deck and Home Sprayer is recommended for its versatile use in cleaning, sealing, lawn and garden application, and insect control. The high performance foaming nozzle and adjustable nozzles offer flexibility in spraying applications.',
-            'related_items': []}
-           ]
